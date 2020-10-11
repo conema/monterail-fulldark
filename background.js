5fd170c013edf376de25e2fd34febcd04da47b45
Emanuele Concas <conema@outlook.com>
2020-10-11 19:48:43 +0200

    Options page

--- background.js
+++ background.js
@@ -1,33 +1,33 @@
-messenger.WindowListener.registerChromeUrl([ 
-    ["content",  "MonterailFDOverlay", "content/"]
+messenger.WindowListener.registerChromeUrl([
+    ["content", "MonterailFDOverlay", "content/"]
 ]);
 
 messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/addressbook/addressbook.xhtml", 
+    "chrome://messenger/content/addressbook/addressbook.xhtml",
     "chrome://MonterailFDOverlay/content/scripts/loadAddressbook.js");
 
 messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/addressbook/abContactsPanel.xhtml", 
+    "chrome://messenger/content/addressbook/abContactsPanel.xhtml",
     "chrome://MonterailFDOverlay/content/scripts/loadAddressbook.js");
 
 messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/messengercompose/messengercompose.xhtml", 
+    "chrome://messenger/content/messengercompose/messengercompose.xhtml",
     "chrome://MonterailFDOverlay/content/scripts/loadCompose.js");
 
 messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/messenger.xhtml", 
+    "chrome://messenger/content/messenger.xhtml",
     "chrome://MonterailFDOverlay/content/scripts/loadMessenger.js");
 
 messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/messageWindow.xhtml", 
+    "chrome://messenger/content/messageWindow.xhtml",
     "chrome://MonterailFDOverlay/content/scripts/loadMessenger.js");
 
 messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/customizeToolbar.xhtml", 
+    "chrome://messenger/content/customizeToolbar.xhtml",
     "chrome://MonterailFDOverlay/content/scripts/loadMessenger.js");
 
 messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/glodaFacetView.xhtml", 
+    "chrome://messenger/content/glodaFacetView.xhtml",
     "chrome://MonterailFDOverlay/content/scripts/loadSearch.js");
 
 messenger.WindowListener.startListening();
---
+++ content/scripts/options.js
@@ -0,0 +1,52 @@
+window.browser = window.browser.extension.getBackgroundPage().browser;
+
+const defaultMessageListHeight = "40";
+const defaultFolderTreeHeight = "25";
+
+var messageListInput = document.querySelector("#messageListHeight");
+var folderTreeInput = document.querySelector("#folderTreeHeight");
+
+function saveOptions(e) {
+    browser.storage.sync.set({
+        messageListHeight: messageListInput.value,
+        folderTreeHeight: folderTreeInput.value
+    });
+    e.preventDefault();
+
+    if (messageListInput.validationMessage || folderTreeInput.validationMessage){
+        document.querySelector("#error").style.display = "block";
+        document.querySelector("#error").innerHTML = messageListInput.validationMessage + "<br>" + folderTreeInput.validationMessage;
+        document.querySelector("#success").style.display = "none";
+    } else {
+        document.querySelector("#error").style.display = "none";
+        document.querySelector("#success").style.display = "block";
+        document.querySelector("#success").innerHTML = "Settings saved. You must restart Thunderbird to see the changes.";
+    }
+}
+
+function resetOptions(e) {
+    browser.storage.sync.set({
+        messageListHeight: defaultMessageListHeight,
+        folderTreeHeight: defaultFolderTreeHeight
+    });
+    e.preventDefault();
+
+    messageListInput.value = defaultMessageListHeight;
+    folderTreeInput.value = defaultFolderTreeHeight;
+
+    document.querySelector("#error").style.display = "none";
+    document.querySelector("#success").style.display = "block";
+    document.querySelector("#success").innerHTML = "Settings are restored. You must restart Thunderbird to see the changes.";
+}
+
+function restoreOptions() {
+    var gettingItem = browser.storage.sync.get(['messageListHeight', 'folderTreeHeight']);
+    gettingItem.then((res) => {
+        messageListInput.value = res.messageListHeight || defaultMessageListHeight;
+        folderTreeInput.value = res.folderTreeHeight || defaultFolderTreeHeight;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("#submit").addEventListener("click", saveOptions);
+document.querySelector("#reset").addEventListener("click", resetOptions);
